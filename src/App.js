261eefdb07a4dc3ede19b8c136f25bc7b595c394
Arpit <agvinayak27@gmail.com>
2023-04-15 19:07:15 +0530

    Initial router setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
 import Divider from './components/Divider/Divider';
 import Header from './components/Header/Header';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Vehicle List!</div>,
+  },
+  {
+    path:"/tollList",
+    element: <div>tollListPage</div>,
+
+  }
+]);
+
+
 
 function App() {
   return (
     <div className="App">
     <Header/>
     <Divider/>
+    <RouterProvider router={router} />
     </div>
   );
 }
+
 
 export default App;
