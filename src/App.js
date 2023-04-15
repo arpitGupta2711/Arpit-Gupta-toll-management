506d55f6015836101c19349c8e23814839421a25
Arpit <agvinayak27@gmail.com>
2023-04-15 18:41:52 +0530

    Added Header and a horizontal divider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import Divider from './components/Divider/Divider';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-    Toll Management Application
+    <Header/>
+    <Divider/>
     </div>
   );
 }
---
+++ src/components/Divider/Divider.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+const Divider = () => {
+  return (
+    <div class="divider"></div>
+  )
+}
+
+export default Divider
